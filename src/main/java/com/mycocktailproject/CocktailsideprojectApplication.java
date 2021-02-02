c1667d7eb3b2041c449443220b9f4393976d0da5
Michael Drew <michaeljdrew14@gmail.com>
2021-02-02 13:55:21 -0500

    initial commit

---
+++ src/main/java/com/mycocktailproject/CocktailsideprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mycocktailproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailsideprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailsideprojectApplication.class, args);
+	}
+
+}
