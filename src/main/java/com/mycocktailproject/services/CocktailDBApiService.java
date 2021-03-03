47ac8816b8deda67b49ec90ee17d726c70a46466
Jaimie Sanita <jsanita11@gmail.com>
2021-03-03 15:58:50 -0500

    Added exceptions to server

--- src/main/java/com/mycocktailproject/services/CocktailDBApiService.java
+++ src/main/java/com/mycocktailproject/services/CocktailDBApiService.java
@@ -5,86 +5,98 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import com.mycocktailproject.models.CocktailDBCocktail;
 import com.mycocktailproject.models.CocktailDBWrapper;
 import com.mycocktailproject.models.CocktailPreview;
+
 @Component
 public class CocktailDBApiService {
-	
+
 	private final RestTemplate restTemplate = new RestTemplate();
 	private final String BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1";
-	
 
-	
 	public CocktailDBCocktail getCocktailById(long id) {
 		String queryUrl = BASE_URL + "/lookup.php?i={i}";
 		Map<String, String> params = new HashMap<>();
-		params.put("i", ""+ id); 
+		params.put("i", "" + id);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 		HttpEntity input = new HttpEntity<>(headers);
-		HttpEntity<CocktailDBWrapper> resultEntity = restTemplate.exchange(queryUrl, HttpMethod.GET, input, CocktailDBWrapper.class, params);
-		CocktailDBWrapper result = resultEntity.getBody();
-		if(result != null && result.getDrinks() != null && result.getDrinks().length ==1) {
-			return result.getDrinks()[0];
-		} else {
-			//this is where you would throw an exception 404
-			return null;
+		try {
+			HttpEntity<CocktailDBWrapper> resultEntity = restTemplate.exchange(queryUrl, HttpMethod.GET, input,
+					CocktailDBWrapper.class, params);
+			CocktailDBWrapper result = resultEntity.getBody();
+			if (result != null && result.getDrinks() != null && result.getDrinks().length == 1) {
+				return result.getDrinks()[0];
+			}
+		} catch (RestClientResponseException ex) {
+			System.out.println(ex.getRawStatusCode() + " : " + ex.getStatusText());
+		} catch (ResourceAccessException ex) {
+			System.out.println(ex.getMessage());
 		}
-		
-		
+		return null;
 	}
+
 	public CocktailDBCocktail[] getCocktailByName(String name) {
 		String queryUrl = BASE_URL + "/search.php?s={name}";
 		Map<String, String> params = new HashMap<>();
-		params.put("name", ""+ name); 
+		params.put("name", "" + name);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 		HttpEntity input = new HttpEntity<>(headers);
-		HttpEntity<CocktailDBWrapper> resultEntity = restTemplate.exchange(queryUrl, HttpMethod.GET, input, CocktailDBWrapper.class, params);
-		CocktailDBWrapper result = resultEntity.getBody();
-		if(result != null && result.getDrinks() != null) {
-			return result.getDrinks();
-		} else {
-			//this is where you would throw an exception 404
-			return null;
+		try {
+			HttpEntity<CocktailDBWrapper> resultEntity = restTemplate.exchange(queryUrl, HttpMethod.GET, input,
+					CocktailDBWrapper.class, params);
+			CocktailDBWrapper result = resultEntity.getBody();
+			if (result != null && result.getDrinks() != null) {
+				return result.getDrinks();
+			}
+		} catch (RestClientResponseException ex) {
+			System.out.println(ex.getRawStatusCode() + " : " + ex.getStatusText());
+		} catch (ResourceAccessException ex) {
+			System.out.println(ex.getMessage());
 		}
-		
-		
+		return null;
 	}
+
 	public CocktailPreview[] getCocktailPreviewByIngredient(String ingredient) {
 		String queryUrl = BASE_URL + "/filter.php?i={ingredient}";
 		Map<String, String> params = new HashMap<>();
-		params.put("ingredient", ""+ ingredient); 
+		params.put("ingredient", "" + ingredient);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 		HttpEntity input = new HttpEntity<>(headers);
-		HttpEntity<CocktailDBWrapper> resultEntity = restTemplate.exchange(queryUrl, HttpMethod.GET, input, CocktailDBWrapper.class, params);
-		CocktailDBWrapper result = resultEntity.getBody();
-		if(result != null && result.getDrinks() != null) {
-			CocktailPreview[] previews = new CocktailPreview[result.getDrinks().length];
-			for(int i = 0; i< previews.length; i++) {
-				CocktailPreview cocktailPreview = new CocktailPreview();
-				CocktailDBCocktail db = result.getDrinks()[i];
-				cocktailPreview.setName(db.getStrDrink());
-				cocktailPreview.setId(db.getIdDrink());
-				cocktailPreview.setPhotoUrl(db.getStrDrinkThumb());
-				previews[i] = cocktailPreview;
+		try {
+			HttpEntity<CocktailDBWrapper> resultEntity = restTemplate.exchange(queryUrl, HttpMethod.GET, input,
+					CocktailDBWrapper.class, params);
+			CocktailDBWrapper result = resultEntity.getBody();
+			if (result != null && result.getDrinks() != null) {
+				CocktailPreview[] previews = new CocktailPreview[result.getDrinks().length];
+				for (int i = 0; i < previews.length; i++) {
+					CocktailPreview cocktailPreview = new CocktailPreview();
+					CocktailDBCocktail db = result.getDrinks()[i];
+					cocktailPreview.setName(db.getStrDrink());
+					cocktailPreview.setId(db.getIdDrink());
+					cocktailPreview.setPhotoUrl(db.getStrDrinkThumb());
+					previews[i] = cocktailPreview;
+				}
+				return previews;
+
 			}
-			return previews;
-		} else {
-			//this is where you would throw an exception 404
-			return null;
+		} catch (RestClientResponseException ex) {
+			System.out.println(ex.getRawStatusCode() + " : " + ex.getStatusText());
+		} catch (ResourceAccessException ex) {
+			System.out.println(ex.getMessage());
 		}
-		
-		
+		return null;
 	}
 }
