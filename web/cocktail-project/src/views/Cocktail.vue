15b7f620b06292431ede6a48c2f5b114cab325b1
Michael Drew <michaeljdrew14@gmail.com>
2021-03-07 17:26:15 -0500

    ?

--- web/cocktail-project/src/views/Cocktail.vue
+++ web/cocktail-project/src/views/Cocktail.vue
@@ -1,5 +1,11 @@
 <template>
+  
   <div id="background" :style="{'background-image':'url(https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80)'}">> 
+<div id="nav">
+      <router-link to="/">Home</router-link> |
+     
+    </div>
+
 </div>
 </template>
 
