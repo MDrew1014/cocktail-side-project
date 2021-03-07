477654c613f5cea622d06b7781220c061571cff5
Michael Drew <michaeljdrew14@gmail.com>
2021-03-07 16:10:00 -0500

    partay

--- web/cocktail-project/src/App.vue
+++ web/cocktail-project/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app" >
+    
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/cocktail">Cocktail Partay</router-link>
     </div>
     <router-view/>
   </div>
@@ -29,4 +30,5 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
---
+++ web/cocktail-project/src/views/Cocktail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="background" :style="{'background-image':'url(https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80)'}">> 
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#background{
+  position: fixed;
+    top: 0;
+    bottom:0;
+    left:0;
+    
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+  background-size: auto;
+  
+}
+</style>
